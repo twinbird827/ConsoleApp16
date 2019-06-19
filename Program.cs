7f957c4d42988fd8b1f686269ff03e207e0a1a1b
twinbird827 <twinbird827@gmail.com>
2019-06-19 16:17:25 +0900

    [new] %APPDATA%周りの不具合確認用

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var s2 = Path.Combine(s1, "test");
+            var s3 = Path.Combine(s2, "test.txt");
+
+            Directory.CreateDirectory(s2);
+
+            while (true)
+            {
+                File.AppendAllText(s3, "a");
+                Console.ReadLine();
+            }
+        }
+    }
+}
