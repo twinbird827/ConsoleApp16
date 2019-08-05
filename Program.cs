f3c9fddfe1323496f810408ccac3b51d7a42292d
twinbird827 <twinbird827@gmail.com>
2019-08-05 15:09:48 +0900

    [new] AppData上のファイルとOfficeの仮想フォルダの関係性確認用プログラム

--- Program.cs
+++ Program.cs
@@ -11,17 +11,19 @@ namespace ConsoleApp16
     {
         static void Main(string[] args)
         {
-            var s1 = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var s2 = Path.Combine(s1, "test");
-            var s3 = Path.Combine(s2, "test.txt");
+            //var s1 = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            //var s2 = Path.Combine(s1, "test");
+            //var s3 = Path.Combine(s2, "test.txt");
 
-            Directory.CreateDirectory(s2);
+            //Directory.CreateDirectory(s2);
 
-            while (true)
-            {
-                File.AppendAllText(s3, "a");
-                Console.ReadLine();
-            }
+            //while (true)
+            //{
+            //    File.AppendAllText(s3, "a");
+            //    Console.ReadLine();
+            //}
+            var file = @"C:\Temp\test\test.txt";
+            File.Delete(file);
         }
     }
 }
